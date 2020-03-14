8595d1e445cb1651a23a1dfa74b55662a1d9df2c
dennyschico1 <dennyschico2@gmail.com>
2020-03-14 04:45:21 +0100

    auth interceptor fializado

--- src/app/Servicios/user.service.ts
+++ src/app/Servicios/user.service.ts
@@ -2,26 +2,33 @@ import { Injectable } from '@angular/core';
 
 import {HttpClient} from '@angular/common/http'
 import {Observable} from 'rxjs'
+
+const API_URL='http://localhost:8080/trabajos/api/test/';
+
 @Injectable({
 
   providedIn: 'root'
 })
+ 
 export class UserService {
-
+ 
   constructor(private http:HttpClient ) { }
 
-  const API_URL='http://localhost:8080/trabajos/api/test/';
   getPublicContent(): Observable<any> {
     return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
 
   getUserBoard(): Observable<any> {
-    return this.http.get(API_URL + 'user', { responseType: 'text' });
+    return this.http.get(API_URL + 'user', {
+       responseType: 'text' });
   }
 
   getModeratorBoard(): Observable<any> {
-    return this.http.get(API_URL + 'mod', { responseType: 'text' });
+    return this.http.get(API_URL + 'mod', {
+       responseType: 'text' });
   }
+
+
 
   getAdminBoard(): Observable<any> {
     return this.http.get(API_URL + 'admin', { responseType: 'text' });
---
+++ src/app/_helpers/AuthInterceptor.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
+import {HTTP_INTERCEPTORS,HttpHandler,HttpRequest} from '@angular/common/http'
+
+import  {TokenStorageService} from '../Servicios/token-storage.service'
+
+const TOKEN_HEADER_KEY = 'Authorization';
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+   
+   
+    constructor(private token:TokenStorageService ){
+
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        let authReq=req;
+        const token =this.token.getToken();
+        if(token !=null){
+            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+        }
+        return next.handle(authReq);
+    }
+    
+   
+
+
+    /*
+    import { HTTP_INTERCEPTORS } from '@angular/common/http';
+    import { Injectable } from '@angular/core';
+    import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+    import { TokenStorageService } from '../_services/token-storage.service';
+    */
+
+}
+
+//FOR NODE JS
+
+/*
+    const TOKEN_HEADER_KEY = 'x-access-token';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  ...
+  intercept(req: HttpRequest, next: HttpHandler) {
+    ...
+    if (token != null) {
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
+    }
+    return next.handle(authReq);
+  }
+}
+
+*/
+export const  AuthInterceptorProvider=[
+    {provider:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true }
+ ]
